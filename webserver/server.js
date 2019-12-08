c3ffd3d280298d8c74376d3f019b25f197941035
Russ Sayers <russell.sayers@gmail.com>
2019-12-07 18:43:43 -0800

    switched to Kafka.createReadStream API. the prev api was introducing some randomness https://github.com/Blizzard/node-rdkafka/blob/v2.7.4/src/workers.cc#L444

--- webserver/server.js
+++ webserver/server.js
@@ -1,38 +1,32 @@
 const Kafka = require('node-rdkafka');
 const bootstrapServers = process.env.BOOTSTRAP_SERVERS || 'localhost:29092';
 
-const consumer = new Kafka.KafkaConsumer({
+var stream = Kafka.createReadStream({
   'group.id': 'webserver-01',
-  'metadata.broker.list': bootstrapServers,
-}, {});
+  'metadata.broker.list': bootstrapServers
+}, {}, {
+  topics: ['driver-positions-distance'],
+  waitInterval: 0
+});
 
-consumer.connect();
+stream.on('data', function(data) {
+    const arr = data.value.toString().split(',');
+    const message = {
+      'key': data.key.toString(),
+      'latitude': parseFloat(arr[0]).toFixed(6),
+      'longitude': parseFloat(arr[1]).toFixed(6),
+      'timestamp': data.timestamp,
+      // "latency" : new Date().getTime() - data.timestamp,
+      'partition': data.partition,
+      'offset': data.offset,
+    };
 
-consumer
-    .on('ready', function() {
-      console.log('Consumer ready');
-      consumer.subscribe(['driver-positions-distance']);
-      consumer.consume();
-    })
-    .on('data', function(data) {
-      const arr = data.value.toString().split(',');
-      const message = {
-        'key': data.key.toString(),
-        'latitude': parseFloat(arr[0]).toFixed(6),
-        'longitude': parseFloat(arr[1]).toFixed(6),
-        'timestamp': data.timestamp,
-        // "latency" : new Date().getTime() - data.timestamp,
-        'partition': data.partition,
-        'offset': data.offset,
-      };
+    if (arr.length > 2) {
+      message['distance'] = Math.round(arr[2]);
+    }
 
-      if (arr.length > 2) {
-        message['distance'] = Math.round(arr[2]);
-      }
-
-      io.sockets.emit('new message', message);
-    });
-
+    io.sockets.emit('new message', message);
+  });
 
 // Setup basic express server
 const express = require('express');
