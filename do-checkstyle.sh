398b21f1a0903c22512bf37bdf14473f5f0bbb43
Russ Sayers <russell.sayers@gmail.com>
2019-12-17 08:27:01 -0800

    linting ALL THE THINGS

--- do-checkstyle.sh
+++ do-checkstyle.sh
@@ -2,3 +2,6 @@
 java -jar checkstyle/checkstyle-8.24-all.jar -c checkstyle/google_checks.xml java-producer/src/
 java -jar checkstyle/checkstyle-8.24-all.jar -c checkstyle/google_checks.xml streams/src/
 java -jar checkstyle/checkstyle-8.24-all.jar -c checkstyle/google_checks.xml java-producer-avro/src/
+pushd webserver ; npm run pretest ; popd
+pushd webserver-avro ; npm run pretest ; popd
+# pushd node-producer ; npm run pretest ; popd
--- node-producer/server.js
+++ node-producer/server.js
@@ -1,25 +1,21 @@
 const Kafka = require('node-rdkafka');
 
-var bootstrapServers = process.env.BOOTSTRAP_SERVERS || "localhost:29092";
-
-
 const producer = new Kafka.Producer({
-    'client.id': 'node-producer',
-    'metadata.broker.list': bootstrapServers,
-    'dr_cb': true
-},{
-  'partitioner' : 'murmur2_random'
+  'client.id': 'node-producer',
+  'metadata.broker.list': 'kafka:9092',
+  'dr_cb': true,
+}, {
+  'partitioner': 'murmur2_random',
 });
 
-var driverId = process.argv[2];
-var driverData = process.argv[3];
+const driverId = process.argv[2];
+const driverData = process.argv[3];
 
 const fs = require('fs');
-let rawdata = fs.readFileSync(driverData);
-let coords = JSON.parse(rawdata);
-let lasttime = Date.now();
+const rawdata = fs.readFileSync(driverData);
+const coords = JSON.parse(rawdata);
 let pos = 0;
-  
+
 producer.setPollInterval(100);
 producer.connect();
 
@@ -27,22 +23,22 @@ producer.on('ready', () => {
   console.log(`Producer ready: ${driverId} ${driverData}`);
   setInterval(() => {
     try {
-        const key = driverId;
-        const value = coords[pos][0] + "," + coords[pos][1];
-        producer.produce(
-            'driver-positions',
-            null,
-            Buffer.from(value),
-            key,
-            Date.now()
-        );
-        pos = (pos + 1) % coords.length;
+      const key = driverId;
+      const value = coords[pos][0] + ',' + coords[pos][1];
+      producer.produce(
+          'driver-positions',
+          null,
+          Buffer.from(value),
+          key,
+          Date.now(),
+      );
+      pos = (pos + 1) % coords.length;
     } catch (err) {
-        client.end(true);
-        console.error('A problem occurred when sending our message');
-        console.error(err);
-    }    
-  }, 1000)
+      client.end(true);
+      console.error('A problem occurred when sending our message');
+      console.error(err);
+    }
+  }, 1000);
 });
 
 producer.on('delivery-report', (err, report) => {
@@ -50,7 +46,7 @@ producer.on('delivery-report', (err, report) => {
   // console.log(report);
 });
 
-producer.on('event.error', err => {
-    console.error('Error from producer');
-    console.error(err);
-})
+producer.on('event.error', (err) => {
+  console.error('Error from producer');
+  console.error(err);
+});
