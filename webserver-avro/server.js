e85872de65f9a86fe57aa7ca47ba53ee627d2ccb
Russ Sayers <russell.sayers@gmail.com>
2019-12-27 08:26:11 -0800

    passing topic via env variable, now using the same code for 3 topics.

--- webserver-avro/server.js
+++ webserver-avro/server.js
@@ -1,14 +1,3 @@
-// /////// using node-rdkafka and avsc to do the desearialization myself
-// const Kafka = require('node-rdkafka');
-// const avro = require('avsc');
-// const type = avro.Type.forSchema({
-//   type: "record",
-//   fields: [
-//       {"name": "latitude", "type": "double"},
-//       {"name": "longitude", "type": "double"}
-//   ]
-// });
-// ///////
 const KafkaAvro = require('kafka-avro');
 const kafkaAvro = new KafkaAvro({
   kafkaBroker: 'kafka:9092',
@@ -19,48 +8,11 @@ kafkaAvro.init()
       console.log('Ready to use');
     });
 
-// ///////
-
 const topics = [
-  'driver-positions-avro',
-  'driver-augmented-avro',
+  process.env.TOPIC
 ];
-let maxTopicIndex = 0;
 
-// /////// doing the desearialization myself
-// const stream = Kafka.createReadStream({
-//   'group.id': 'webserver-01',
-//   'metadata.broker.list': bootstrapServers,
-// }, {'auto.offset.reset': 'earliest'}, {
-//   topics: topics,
-//   waitInterval: 0,
-// });
-//
-// stream.on('data', function(data) {
-//   const topicIndex = topics.indexOf(data.topic);
-//   maxTopicIndex = Math.max(topicIndex, maxTopicIndex);
-//
-//   // https://github.com/mtth/avsc/issues/29#issuecomment-175196709
-//   const val = type.decode(data.value, 5).value;
-//   console.log(val.latitude, val.longitude);
-//
-//   if (topicIndex==maxTopicIndex) {
-//     const arr = data.value.toString().split(',');
-//     const message = {
-//       'topic': data.topic,
-//       'key': data.key.toString(),
-//       'latitude': val.latitude,
-//       'longitude': val.longitude,
-//       'timestamp': data.timestamp,
-//       'partition': data.partition,
-//       'offset': data.offset,
-//     };
-//
-//     io.sockets.emit('new message', message);
-//   }
-// });
-// ///////
-
+console.log("subscribing to ", topics);
 kafkaAvro.getConsumerStream({
   'group.id': process.env.HOSTNAME,
   'metadata.broker.list': 'kafka:9092',
@@ -71,37 +23,33 @@ kafkaAvro.getConsumerStream({
     .then(function(stream) {
       stream.on('data', function(data) {
         const topicIndex = topics.indexOf(data.topic);
-        maxTopicIndex = Math.max(topicIndex, maxTopicIndex);
+        const message = {
+          'topic': data.topic,
+          'key': data.key.toString(),
+          'timestamp': data.timestamp,
+          'partition': data.partition,
+          'offset': data.offset,
+        };
+        
+        message['latitude'] = data.parsed.latitude || data.parsed.LATITUDE.double;
+        message['longitude'] = data.parsed.longitude || data.parsed.LONGITUDE.double;
 
-        if (topicIndex==maxTopicIndex) {
-          const message = {
-            'topic': data.topic,
-            'key': data.key.toString(),
-            'timestamp': data.timestamp,
-            'partition': data.partition,
-            'offset': data.offset,
-          };
-
-          if (data.topic == 'driver-positions-avro') {
-            message['latitude'] = data.parsed.latitude;
-            message['longitude'] = data.parsed.longitude;
-          }
-
-          // different format for ksql avro stream
-          if (data.topic == 'driver-augmented-avro') {
-            message['latitude'] = data.parsed.LATITUDE.double;
-            message['longitude'] = data.parsed.LONGITUDE.double;
-            message['firstname'] = data.parsed.FIRSTNAME ?
-               data.parsed.FIRSTNAME.string : null;
-            message['lastname'] = data.parsed.LASTNAME ?
-               data.parsed.LASTNAME.string : null;
-            message['make'] = data.parsed.MAKE ?
-                data.parsed.MAKE.string : null;
-            message['model'] = data.parsed.MODEL ?
-                data.parsed.MODEL.string : null;
-          }
-          io.sockets.emit('new message', message);
+        if (data.topic == 'driver-distance-avro') {
+          message['distance'] = Math.round(data.parsed.distance);
         }
+
+        // different format for ksql avro stream
+        if (data.topic == 'driver-augmented-avro') {
+          message['firstname'] = data.parsed.FIRSTNAME ?
+             data.parsed.FIRSTNAME.string : null;
+          message['lastname'] = data.parsed.LASTNAME ?
+             data.parsed.LASTNAME.string : null;
+          message['make'] = data.parsed.MAKE ?
+              data.parsed.MAKE.string : null;
+          message['model'] = data.parsed.MODEL ?
+              data.parsed.MODEL.string : null;
+        }
+        io.sockets.emit('new message', message);
       });
     });
 
