3cc5b8419b9b5ebb4175ce7126ce38c40cc52d05
Russ Sayers <russell.sayers@gmail.com>
2019-12-29 15:06:21 -0800

    working python avro producer

--- python-producer-avro/main.py
+++ python-producer-avro/main.py
@@ -7,18 +7,22 @@ from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 
 DRIVER_FILE_PREFIX = "./drivers/"
-KAFKA_TOPIC = "driver-positions"
+KAFKA_TOPIC = "driver-positions-pyavro"
 DRIVER_ID = os.getenv("DRIVER_ID", "driver-3")
 
-print("Starting Producer (Python)")
+print("Starting Avro Producer (Python)")
 
 value_schema = avro.load("position_value.avsc")
+key_schema = avro.load("position_key.avsc")
 
 producer = AvroProducer({
     'bootstrap.servers': 'kafka:9092',
+    'plugin.library.paths': 'monitoring-interceptor',
     'partitioner': 'murmur2_random',
     'schema.registry.url': 'http://schema-registry:8081'
-}, default_value_schema=value_schema)
+}
+                        , default_key_schema=key_schema
+                        , default_value_schema=value_schema)
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -26,8 +30,7 @@ def delivery_report(err, msg):
     if err is not None:
         print('Message delivery failed: {}'.format(err))
     else:
-        print(' Wrote message [partition {}]'.format(msg.partition()))
-
+        print('Mesassge written [partition {}]'.format(msg.partition()))
 
 def exit_handler():
     """Run this on exit"""
@@ -46,10 +49,19 @@ while True:
     line = lines[pos]
     # Trigger any available delivery report callbacks from previous produce() calls
     producer.poll(0)
+    latitude = line.split(",")[0].strip()
+    longitude = line.split(",")[1].strip()
     producer.produce(
         topic=KAFKA_TOPIC,
-        value={"latitude" : 30.00001, "longitude" : 40.000003},
-        key=DRIVER_ID.encode('utf-8'),
+        value={"latitude" : float(latitude), "longitude" : float(longitude)},
+        key={"key" : DRIVER_ID},
         callback=delivery_report)
     sleep(1)
     pos = (pos + 1) % len(lines)
+
+# Confirm the topic is being written to with kafka-avro-console-consumer
+#
+# kafka-avro-console-consumer --bootstrap-server kafka:9092 \
+#     --property schema.registry.url=http://schema-registry:8081 \
+#     --topic driver-positions-pyavro --property print.key=true \
+# --from-beginning
